96ffb87d65f434f46348bfc67cb9869383456bc8
Trenton200913 <111209161+Trenton200913@users.noreply.github.com>
2024-01-05 09:53:06 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+game.splash("Error-2024", "We Moved to unity. Reason: We got upgraded gear. Error 1221, We are remaking are games made with this engine with Unity. For more Info Send me a email. trentonbrunston10@outlook.com")
+forever(function () {
+    music.play(music.createSoundEffect(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    pause(500)
+    music.play(music.createSoundEffect(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    pause(500)
+    music.play(music.createSoundEffect(WaveShape.Noise, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
